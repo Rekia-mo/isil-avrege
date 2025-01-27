ab7f39ba71688731f5b1c0338e80aa7b5e90ea1b
reeka <rmansourouahchia@gmail.com>
2025-01-27 07:38:54 -0800

    Update avrage.js

--- avrage.js
+++ avrage.js
@@ -7,6 +7,10 @@ const modules = [{
   cof: 3
 },
 {
+  name: 'POO',
+  cof: 2
+},
+{
   name: 'LOGIC',
   cof: 2,
 },
@@ -23,10 +27,6 @@ const modules = [{
   cof: 2,
 },
 {
-  name: 'OOP',
-  cof: 2,
-},
-{
   name: 'ENG',
   cof: 1,
 }];
@@ -36,7 +36,7 @@ const modules = [{
 let HTML1 = '';
 modules.forEach((module)=>{
   
-  if(module.name === 'ALGO' || module.name === 'OOP'){
+  if(module.name === 'ALGO' || module.name === 'POO'){
     HTML1+=`
     <div class="module" data-module-name="${module.name}">
       <legend class="module-name">${module.name}:</legend>
@@ -84,7 +84,7 @@ modules.forEach((module)=>{
 }); 
 
 document.querySelector('.js-moduls').innerHTML = HTML1;
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function som(modules){
   let som = 0;
   modules.forEach((module)=>{
@@ -93,7 +93,6 @@ function som(modules){
   return som;
 }
 
-///////////////////////////
 function calc(){
   
   const somCof = som(modules);
@@ -101,7 +100,7 @@ function calc(){
   let doc1 = document.querySelector('.js-moys');
   doc1.innerHTML = '';
 
-  let condition = true;
+   condition = true;
   let total = 0;
 
   document.querySelectorAll('.module').forEach((module)=>{
@@ -113,10 +112,10 @@ function calc(){
 
     if(moduleName === 'ENG'){
       //Bring the exam's val
-      const noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
+      let noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
 
       if(noteExam < 0 || noteExam > 20 || isNaN(noteExam)){
-
+        noteExam = 0; 
         condition = false;
 
         document.querySelector(`.worning-exam-${moduleName}`).innerHTML = `*Invalid marks for ${moduleName}. Enter marks between 0 and 20.`
@@ -130,13 +129,13 @@ function calc(){
     }else 
     
     
-    if(moduleName === 'ALGO' || moduleName === 'OOP'){
-      const noteTd = Number(document.querySelector(`.td-${moduleName}`).value);
+    if(moduleName === 'ALGO' || moduleName === 'POO'){
+      let noteTd = Number(document.querySelector(`.td-${moduleName}`).value);
 
       if(noteTd < 0 || noteTd > 20 || isNaN(noteTd)){
-
+        noteTd = 0;
         condition = false;
-
+        
         document.querySelector(`.worning-td-${moduleName}`).innerHTML = `*Invalid marks for ${moduleName}. Enter marks between 0 and 20.`
 
       }else{
@@ -146,10 +145,10 @@ function calc(){
 
 
       //Bring the exam's val
-      const noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
+      let noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
 
       if(noteExam < 0 || noteExam > 20 || isNaN(noteExam)){
-
+        noteExam = 0;
         condition = false;
 
         document.querySelector(`.worning-exam-${moduleName}`).innerHTML = `*Invalid marks for ${moduleName}. Enter marks between 0 and 20.` 
@@ -159,10 +158,10 @@ function calc(){
 
 
       //Bring the tp's val
-      const noteTp = Number(document.querySelector(`.tp-${moduleName}`).value);
+      let noteTp = Number(document.querySelector(`.tp-${moduleName}`).value);
 
       if(noteTp < 0 || noteTp > 20 || isNaN(noteTp)){
-
+        noteTp = 0;
         condition = false;
 
         document.querySelector(`.worning-tp-${moduleName}`).innerHTML = `*Invalid marks for ${moduleName}. Enter marks between 0 and 20.`;
@@ -178,11 +177,11 @@ function calc(){
     
     {
       //Bring the td's val
-      const noteTd = Number( document.querySelector(`.td-${moduleName}`).value);
+      let noteTd = Number( document.querySelector(`.td-${moduleName}`).value);
       
       //condition
       if(noteTd < 0 || noteTd > 20 || isNaN(noteTd)){
-
+        noteTd = 0;
         condition = false;
 
         document.querySelector(`.worning-td-${moduleName}`).innerHTML = `*Invalid marks for ${moduleName}. Enter marks between 0 and 20.`
@@ -191,10 +190,10 @@ function calc(){
       }
 
       //Bring the exam's val
-      const noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
+      let noteExam = Number(document.querySelector(`.exam-${moduleName}`).value);
       if(noteExam < 0 || noteExam > 20 || isNaN(noteExam)){
         condition = false;
-
+        noteExam = 0;
         document.querySelector(`.worning-exam-${moduleName}`).innerHTML = `* Invalid marks for ${moduleName}. Enter marks between 0 and 20.`
 
       }else{
@@ -203,26 +202,33 @@ function calc(){
 
       //calc the moy
        moy = (0.4 * noteTd + 0.6 * noteExam).toFixed(2);
-       
     };
 
     doc1.innerHTML += `<section class="section"><div class="moduleName"> ${moduleName} </div> <div class="moduleMoy">${moy}</div></section>`;
-     
-    total += parseFloat(moy) * cof; 
-    
+
+    if(condition === true){
+      total += parseFloat(moy) * cof;
+     }else{
+      total=0;
+     }
+
   });
  
   total = (total / somCof).toFixed(2);
-
+  
   document.querySelector('.js-total').innerHTML = `<div class="total">Total avrege: <nav class="moy" >${total}</nav></div>`;
+
+  //the remark
+  let random = Math.random();
+  remark(random, total);
 
   return condition;
 };
- 
   document.querySelector('.js-sub-buton').addEventListener('click', condition);
-
+ 
 function condition(){
-  if(calc() === false){
+  let x = calc();
+  if(x === false){
     document.querySelector('.js-condition').innerHTML = '<span class="material-symbols-outlined">warning</span>your marks are invalid go back and check your marks';
     return ;
 
@@ -245,5 +251,88 @@ function findCof(moduleName){
   return X;
 }
 
+function remark(random, total){
+
+  if(0 < total && total < 7 && total != 0){
+    if (0 < random && random <= 0.5){
+      document.querySelector('.js-remark').innerHTML = `Well, at least it's not negative ,<i class="fa-solid fa-face-grin-tongue-wink"></i>`;
+      
+    }else if(0.5 < random && random < 1){
+      document.querySelector('.js-remark').innerHTML = `Well, you’re not getting kicked out, so that’s a win!<i class="fa-solid fa-face-laugh-squint"></i>`;
+    }
+
+  }else if(7 <= total && total < 10){
+    if (0 <= random && random < (1/2)){
+      document.querySelector('.js-remark').innerHTML = `Close… just not close enough yet!<i class="fa-solid fa-charging-station"></i>`;
+
+    }else if((1/2) <= random && random < 1){
+      document.querySelector('.js-remark').innerHTML = `Don't worry, even Einstein had to start somewhere!<i class="fa-solid fa-face-smile-wink"></i>`;
+    }
+    
+  }else  if(10 <= total&& total < 13){
+    if (0 <= random && random < (1/3)){
+      document.querySelector('.js-remark').innerHTML = `you did it! let's GOOOOO <i class="fa-solid fa-wind"></i>`;
+
+    }else if((1/3) <= random && random < (2/3)){
+      document.querySelector('.js-remark').innerHTML = `Genius mode: activated. Keep it up!<i class="fa-solid fa-charging-station"></i>`;
+    }else{
+      document.querySelector('.js-remark').innerHTML = `Not bad, Einstein! Almost there.<i class="fa-solid fa-face-grin-tongue-wink"></i>`;
+    }
 
 
+
+  }else if(13 <= total&& total < 15){
+    if (0 <= random && random < (1/3)){
+      document.querySelector('.js-remark').innerHTML = `Not bad, Einstein! Almost there.<i class="fa-solid fa-face-grin-tongue-wink"></i>`;
+
+    }else if((1/3) <= random && random < (2/3)){
+      document.querySelector('.js-remark').innerHTML = `Genius mode: activated. Keep it up!<i class="fa-regular fa-face-grin-stars"></i>`;
+
+    }else{
+      document.querySelector('.js-remark').innerHTML = `Not easy to get those scores in here, but you're making it look simple!<i class="fa-solid fa-face-grin-stars"></i>`;
+
+    }
+  }else if(15 <= total && total < 18){
+    if (0 <= random && random < (1/3)){
+      document.querySelector('.js-remark').innerHTML = `You’re making the impossible look easy!<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+
+    }else if((1/3) <= random && random < (2/3)){
+      document.querySelector('.js-remark').innerHTML = `Another day, another tough score crushed!<i class="fa-regular fa-face-grin-stars"></i>`;
+
+    }else{
+      document.querySelector('.js-remark').innerHTML = `everyone<i class="fa-solid fa-bullhorn"></i>, I'm with the major de promo itself`;
+
+    }
+  }else if(18 <= total && total < 20){
+    if (0 <= random && random < (1/3)){
+      document.querySelector('.js-remark').innerHTML = `Did you just break the system? Because this is insane!<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+
+    }else if((1/3) <= random && random < (2/3)){
+      document.querySelector('.js-remark').innerHTML = `Impossible? Nah, you're joking right?<i class="fa-solid fa-wand-magic-sparkles"></i>;`
+
+    }else{
+      document.querySelector('.js-remark').innerHTML = `You’ve officially done the impossible. Nice!<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+
+    }
+  }else if(total == 20){
+    if (0 <= random && random < (1/3)){
+      document.querySelector('.js-remark').innerHTML = `stop playing and valid your actual marks or I'll retire<i class="fa-solid fa-face-angry"></i>`;
+
+    }else if((1/3) <= random && random < (2/3)){
+      document.querySelector('.js-remark').innerHTML = `stop it bro<i class="fa-solid fa-face-angry"></i>`;
+
+    }else{
+      document.querySelector('.js-remark').innerHTML = `stop wasting my time<i class="fa-solid fa-face-angry"></i>`;
+
+    }
+  }
+}
+
+document.querySelector('.js-id').addEventListener('click', id);
+function id(){
+  document.querySelector('.container2').innerHTML =`<div class="sub-cont"> <spam class="feed">feedback 📣:</spam>
+<p class="dscrbt">Got genius ideas 💡 , hilarious fails 💥🤦‍♂️, or secret thoughts [🤫📨] ?
+<br>  Send them <a class="here" href="mailto:rmansourouahchia@gmail.com">HERE</a> –  " Your opinion matters – whether it’s about cafeteria food 🍕 ". <br>
+Click <a class="here" href="mailto:rmansourouahchia@gmail.com">HERE</a> NOW We’re waiting for your messages! <i class="fa-solid fa-face-smile-wink"></i>.</div> </p>
+  `;
+}
